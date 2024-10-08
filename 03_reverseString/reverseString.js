8b52350ac2bcca71d24f2f736781d5c31f558da8
Ananda Muhammad <anandamuhammad@proton.me>
2024-10-08 18:24:41 +0800

    Submit 03_reverseString solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  const result = str.split("").reverse();
 
+  return result.join("");
 };
 
 // Do not edit below this line
