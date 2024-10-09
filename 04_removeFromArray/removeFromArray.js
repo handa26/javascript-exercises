d131dc799d1f58820aeee430357463e700470b9c
Ananda Muhammad <anandamuhammad@proton.me>
2024-10-09 12:25:42 +0800

    Submit solution for 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,29 @@
-const removeFromArray = function() {
+// const removeFromArray = function(arr, ...removeVal) {
+//   for (let i = 0; i < removeVal.length; i++) {
+//     // Remove array item
+//     if (arr.indexOf(removeVal[i]) !== -1) {
+//       arr.splice(arr.indexOf(removeVal[i]), 1);
+
+//       // Double check
+//       if (arr.indexOf(removeVal[i]) !== -1) {
+//         arr.splice(arr.indexOf(removeVal[i]), 1);
+//       }
+//     }
+//   }
+
+//   return arr;
+// };
+
+const removeFromArray = function(arr, ...removeVal) {
+  const newArray = [];
+  
+  arr.forEach((x) => {
+    if (!removeVal.includes(x)) {
+      newArray.push(x)
+    }
+  });
+
+  return newArray;
 };
 
 // Do not edit below this line
