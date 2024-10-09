e2d9a1fb41adaf88407a79ebc70ce4dc3a7074d3
Ananda Muhammad <anandamuhammad@proton.me>
2024-10-09 18:17:12 +0800

    Submit solution for 05_sumAll exxercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,37 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  const arrOfNum = [];
+  const initialValue = 0;
+  let num1 = x;
+  let num2 = y;
 
+  // Switch number if the first number is bigger the second
+  if (num1 > num2) {
+    num1 = y;
+    num2 = x;
+  }
+
+  // Thrown an error if both args are non integer or is a float number, 
+  if (
+    !(typeof x === "number") ||
+    !(typeof y === "number") ||
+    x < 0 ||
+    y < 0 ||
+    !Number.isInteger(x) ||
+    !Number.isInteger(y)
+  ) {
+    return "ERROR";
+  }
+
+  for (let i = num1; i <= num2; i++) {
+    arrOfNum.push(i);
+  }
+
+  const result = arrOfNum.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue
+  );
+
+  return result;
 };
 
 // Do not edit below this line
