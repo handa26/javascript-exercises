7e9e214bca51e1905ac89975ada70f4f108ec1b4
Ananda Muhammad <anandamuhammad@proton.me>
2024-10-10 14:17:45 +0800

    Submit tempConversion solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  const celsius = (f - 32) / (9 / 5);
+
+  if (!Number.isInteger(celsius)) {
+    return Number(celsius.toFixed(1)); 
+  }
+
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  const fahrenheit = c * 9 / 5 + 32;
+
+  if (!Number.isInteger(fahrenheit)) {
+    return Number(fahrenheit.toFixed(1));
+  }
+
+  return fahrenheit;
 };
 
 // Do not edit below this line
