c331ec67b0a2c124ebf2ea5fe0ff47fcdc89b2cb
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2024-10-22 21:54:13 +0800

    Submit 09_palindromes submission

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const splitStr = str.split("");
+  let reverseStr = "";
 
+  for (let i = splitStr.length - 1; i >= 0; i--) {
+    let char = splitStr[i];
+    reverseStr += char;
+  }
+
+  return reverseStr == str.split('').reverse().join('');
 };
 
 // Do not edit below this line
