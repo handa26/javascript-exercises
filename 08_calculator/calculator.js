6876dfab4df2b85b23cff0f0ab710658f47d5a2e
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2024-10-22 15:28:40 +0800

    Submit 08_calculator submission

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let sum = 1;
+	for (let i = 0; i < arr.length; i++) {
+    sum *= arr[i];
+  }
 
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+  for (i = 1; i <= n; i++) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
