f5a5e7fdb72443859993086db65f8e567ece54af
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2024-10-23 11:29:07 +0800

    Submit 10_fibonacci submission

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  const convertInt = parseInt(n);
+  let result = [];
 
+  if (convertInt < 0) {
+    return "OOPS";
+  };
+
+  if (convertInt === 0) result = [0];
+  if (convertInt === 1) {
+    result = [0, 1];
+  };
+
+  if (convertInt < 2) {
+    for (let i = 1; i <= n; i++) {
+      result.push(i);
+    }
+  } else {
+    result = [0, 1];
+
+    for (let i = 2; i <= convertInt; i++) {
+      result.push(result[result.length - 1] + result[result.length - 2])
+    }
+  }
+
+  return result[n];
 };
 
 // Do not edit below this line
