d6a1fd96f5673d1fd141b51ad931cb3e849f451b
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2024-10-23 15:47:31 +0800

    Submit 11_getTheTitles submission

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
+const getTheTitles = function (obj) {
+  let result = [];
+  obj.map((book) => result.push(book.title));
 
+  return result;
 };
 
 // Do not edit below this line
