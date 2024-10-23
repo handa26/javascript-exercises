11d6503c9c405e95f0efa542a6d47023e5bde0af
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2024-10-23 17:51:31 +0800

    Submit final submission

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let arrOfAges = [];
 
+  const newObj = people.map((person) => {
+    let age = [];
+    let yearOfBirth = person.yearOfBirth;
+    let yearOfDeath = person.yearOfDeath;
+    if (yearOfDeath === undefined) {
+      yearOfDeath = new Date().getFullYear();
+    }
+    for (let i = yearOfBirth; i < yearOfDeath; i++) {
+      person.age = age.push(i);
+    }
+    // console.log(person);
+    return person;
+  })
+
+  newObj.map((person) => arrOfAges.push(person.age));
+
+  const oldestPerson = Math.max(...arrOfAges);
+
+  const result = newObj.filter((person) => {
+    if (person.age === oldestPerson) {
+      return person;
+    }
+  });
+
+  return result[0].name;
 };
 
 // Do not edit below this line
